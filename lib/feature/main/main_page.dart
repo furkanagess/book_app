c42d63f2b4b432524c79489a1a1fbdb40bc36d77
furkanagess <caglarrfurkann@gmail.com>
2023-04-18 01:38:32 +0300

    bottombar

---
+++ lib/feature/main/main_page.dart
@@ -0,0 +1,68 @@
+import 'package:book_app/feature/category/view/category_page_view.dart';
+import 'package:book_app/feature/favorite/view/favorite.dart';
+import 'package:book_app/feature/home/view/book_home_view.dart';
+import 'package:book_app/feature/search/view/book_search_view.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentPage = 0;
+  final pages = [
+    BookHomeView(),
+    BookSearchView(),
+    FavoriteBooksView(),
+    BookCategoriesView(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.amber,
+        unselectedIconTheme: IconThemeData(color: Colors.black),
+        selectedIconTheme: IconThemeData(
+          color: Colors.amber,
+        ),
+        currentIndex: currentPage,
+        type: BottomNavigationBarType.shifting,
+        onTap: (index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.library_add,
+            ),
+            label: "Library",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.book,
+            ),
+            label: "Category",
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
 
 import 'package:book_app/feature/category/view/category_page_view.dart';
-import 'package:book_app/feature/favorite/view/favorite.dart';
 import 'package:book_app/feature/home/view/book_home_view.dart';
+import 'package:book_app/feature/main/main_page.dart';
 import 'package:book_app/feature/search/view/book_search_view.dart';
 import 'package:book_app/feature/favorite/provider/favorite_provider.dart';
 import 'package:flutter/material.dart';
@@ -17,67 +17,15 @@ void main() => runApp(
       ),
     );
 
-class MyApp extends StatefulWidget {
-  MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int currentPage = 0;
-
-  final pages = [
-    const BookHomeView(),
-    const BookSearchView(),
-    const FavoriteBooksView(),
-    BookCategoriesView(),
-  ];
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[currentPage],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.amberAccent,
-        currentIndex: currentPage,
-        type: BottomNavigationBarType.shifting,
-        onTap: (index) {
-          setState(() {
-            currentPage = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-            ),
-            backgroundColor: Colors.amberAccent,
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
-            ),
-            backgroundColor: Colors.amberAccent,
-            label: "Search",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.library_add,
-            ),
-            backgroundColor: Colors.amberAccent,
-            label: "Library",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.book,
-            ),
-            backgroundColor: Colors.amberAccent,
-            label: "Category",
-          ),
-        ],
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: MainPage(),
     );
   }
 }

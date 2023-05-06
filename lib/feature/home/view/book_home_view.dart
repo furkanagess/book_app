d7e58f98cb06b772faa897475a518387f0357e8c
furkanagess <caglarrfurkann@gmail.com>
2023-05-06 20:06:47 +0300

    home_view_model entegreted

--- lib/feature/home/view/book_home_view.dart
+++ lib/feature/home/view/book_home_view.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:book_app/feature/category/view/category_page_view.dart';
+import 'package:book_app/feature/home/viewModel/home_view_model.dart';
 
 import 'package:book_app/feature/search/view/book_search_view.dart';
+import 'package:book_app/product/base/base_view.dart';
 import 'package:book_app/product/constants/api_types.dart';
 import 'package:book_app/product/extensions/context_extension.dart';
 import 'package:book_app/product/models/book.dart';
@@ -40,164 +42,166 @@ class _BookHomeViewState extends State<BookHomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _buildAppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: context.paddingLowHorizontal,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Discover Books",
-                style: context.textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: context.dynamicHeight(0.2),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  color: Colors.amberAccent,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        children: [
-                          Padding(
-                            padding: context.paddingLow,
-                            child: Text(
-                              "Enjoy your reading",
-                              style: context.textTheme.labelLarge,
+    return BaseView<HomeViewModel>(
+      viewModel: HomeViewModel(),
+      onModelReady: (model) {
+        model.setContext(context);
+      },
+      onPageBuilder: (BuildContext context, HomeViewModel homeViewModel) => Scaffold(
+        appBar: _buildAppBar(),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: context.paddingLowHorizontal,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: context.dynamicHeight(0.2),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.amberAccent,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: context.paddingLow,
+                              child: Text(
+                                "Welcome",
+                                style: context.textTheme.headlineSmall,
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: context.paddingLow,
-                            child: Text(
-                              "Enjoy your reading",
-                              style: context.textTheme.labelMedium,
+                            Padding(
+                              padding: context.paddingLow,
+                              child: Text(
+                                "Enjoy your reading. \n You can access the type of books you want \n add the books you want to your favorites \n and look again later",
+                                style: context.textTheme.bodyMedium,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.book,
-                            size: 70,
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: context.dynamicHeight(0.05),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Trendings",
-                    style: context.textTheme.headlineSmall,
-                  ),
-                  Text(
-                    "See more",
-                    style: context.textTheme.bodyMedium,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: context.dynamicHeight(0.3),
-                child: ListView.builder(
-                  itemCount: books.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BookDetailView(book: books[index]),
+                          ],
                         ),
-                      ),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: context.dynamicHeight(0.25),
-                            child: Card(
-                              margin: const EdgeInsets.all(10.0),
-                              elevation: 5,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              semanticContainer: true,
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
-                              child: Image.network(
-                                books[index].thumbnailUrl,
-                                fit: BoxFit.fill,
-                              ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(
+                              Icons.book,
+                              size: 70,
                             ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Recommended",
-                    style: context.textTheme.headlineSmall,
-                  ),
-                  Text(
-                    "See more",
-                    style: context.textTheme.bodyMedium,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: context.dynamicHeight(0.3),
-                child: ListView.builder(
-                  itemCount: books.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BookDetailView(book: books[index]),
+                SizedBox(
+                  height: context.dynamicHeight(0.05),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Trendings",
+                      style: context.textTheme.headlineSmall,
+                    ),
+                    Text(
+                      "See more",
+                      style: context.textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: context.dynamicHeight(0.3),
+                  child: ListView.builder(
+                    itemCount: books.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BookDetailView(book: books[index]),
+                          ),
                         ),
-                      ),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: context.dynamicHeight(0.25),
-                            child: Card(
-                              margin: const EdgeInsets.all(10.0),
-                              elevation: 5,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              semanticContainer: true,
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
-                              child: Image.network(
-                                books[index].thumbnailUrl,
-                                fit: BoxFit.fill,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: context.dynamicHeight(0.25),
+                              child: Card(
+                                margin: const EdgeInsets.all(10.0),
+                                elevation: 5,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                semanticContainer: true,
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                child: Image.network(
+                                  books[index].thumbnailUrl,
+                                  fit: BoxFit.fill,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Recommended",
+                      style: context.textTheme.headlineSmall,
+                    ),
+                    Text(
+                      "See more",
+                      style: context.textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: context.dynamicHeight(0.3),
+                  child: ListView.builder(
+                    itemCount: books.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BookDetailView(book: books[index]),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: context.dynamicHeight(0.25),
+                              child: Card(
+                                margin: const EdgeInsets.all(10.0),
+                                elevation: 5,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                semanticContainer: true,
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                child: Image.network(
+                                  books[index].thumbnailUrl,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/feature/home/viewModel/home_view_model.dart
@@ -0,0 +1,15 @@
+import 'package:book_app/product/base/base_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+part 'home_view_model.g.dart';
+
+class HomeViewModel = _HomeViewModelBase with _$HomeViewModel;
+
+abstract class _HomeViewModelBase with Store, BaseViewModel {
+  @override
+  void setContext(BuildContext context) => viewModelContext = context;
+
+  @override
+  void init() {}
+}

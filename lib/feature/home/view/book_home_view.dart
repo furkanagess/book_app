ec3f0332588a8d0a5c80f49f2b74a54f38fcb518
furkanagess <caglarrfurkann@gmail.com>
2023-06-10 21:47:34 +0300

    color file added & book_home_view fixes

--- lib/feature/home/view/book_home_view.dart
+++ lib/feature/home/view/book_home_view.dart
@@ -4,6 +4,7 @@ import 'package:book_app/feature/home/viewModel/home_view_model.dart';
 
 import 'package:book_app/product/base/base_view.dart';
 import 'package:book_app/product/constants/api_types.dart';
+import 'package:book_app/product/constants/app_colors.dart';
 import 'package:book_app/product/constants/app_strings.dart';
 import 'package:book_app/product/extensions/context_extension.dart';
 import 'package:book_app/product/models/book.dart';
@@ -28,7 +29,7 @@ class _BookHomeViewState extends State<BookHomeView> {
   }
 
   void programmingBooks() async {
-    final response = await http.get(Uri.parse(ApiUrl.programming));
+    final response = await http.get(Uri.parse(ApiUrl.flutterDev));
     if (response.statusCode == 200) {
       final json = jsonDecode(response.body);
       setState(() {
@@ -45,6 +46,7 @@ class _BookHomeViewState extends State<BookHomeView> {
         model.setContext(context);
       },
       onPageBuilder: (BuildContext context, HomeViewModel homeViewModel) => Scaffold(
+        backgroundColor: AppColors().background,
         appBar: _buildAppBar(),
         body: SingleChildScrollView(
           child: Padding(
@@ -52,96 +54,75 @@ class _BookHomeViewState extends State<BookHomeView> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  height: context.dynamicHeight(0.2),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    color: Colors.amberAccent,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: context.paddingLow,
-                              child: Text(
-                                AppStrings.welcome,
-                                style: context.textTheme.headlineSmall,
-                              ),
-                            ),
-                            Padding(
-                              padding: context.paddingLow,
-                              child: Text(
-                                AppStrings.subHeader,
-                                style: context.textTheme.bodyMedium,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(
-                              Icons.book,
-                              size: 70,
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: context.dynamicHeight(0.05),
-                ),
+                headerRow(context),
+                SizedBox(height: context.dynamicHeight(0.05)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
                       AppStrings.trending,
-                      style: context.textTheme.headlineSmall,
+                      style: context.textTheme.headlineSmall?.copyWith(
+                        color: AppColors().white,
+                      ),
                     ),
                     Text(
                       AppStrings.seeMore,
-                      style: context.textTheme.bodyMedium,
+                      style: context.textTheme.bodyMedium?.copyWith(
+                        color: AppColors().white,
+                      ),
                     ),
                   ],
                 ),
                 SizedBox(
-                  height: context.dynamicHeight(0.3),
+                  height: context.dynamicHeight(0.5),
                   child: ListView.builder(
                     itemCount: books.length,
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => BookDetailView(book: books[index]),
+                      return Padding(
+                        padding: context.paddingLow,
+                        child: GestureDetector(
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BookDetailView(book: books[index]),
+                            ),
                           ),
-                        ),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: context.dynamicHeight(0.25),
-                              child: Card(
-                                margin: const EdgeInsets.all(10.0),
-                                elevation: 5,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                semanticContainer: true,
-                                clipBehavior: Clip.antiAliasWithSaveLayer,
-                                child: Image.network(
-                                  books[index].thumbnailUrl,
-                                  fit: BoxFit.fill,
-                                ),
+                          child: Container(
+                            width: context.dynamicWidth(0.44),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: AppColors().transparentWhite,
+                            ),
+                            child: Padding(
+                              padding: context.paddingLow,
+                              child: Column(
+                                children: [
+                                  Card(
+                                    margin: const EdgeInsets.all(10.0),
+                                    elevation: 5,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    semanticContainer: true,
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                    child: Image.network(
+                                      books[index].thumbnailUrl,
+                                      fit: BoxFit.fill,
+                                      height: 200,
+                                      width: 200,
+                                    ),
+                                  ),
+                                  Text(
+                                    books[index].title,
+                                    style: context.textTheme.bodyMedium?.copyWith(
+                                      color: AppColors().white,
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                          ],
+                          ),
                         ),
                       );
                     },
@@ -152,11 +133,15 @@ class _BookHomeViewState extends State<BookHomeView> {
                   children: [
                     Text(
                       AppStrings.recommended,
-                      style: context.textTheme.headlineSmall,
+                      style: context.textTheme.headlineSmall?.copyWith(
+                        color: AppColors().white,
+                      ),
                     ),
                     Text(
                       AppStrings.seeMore,
-                      style: context.textTheme.bodyMedium,
+                      style: context.textTheme.bodyMedium?.copyWith(
+                        color: AppColors().white,
+                      ),
                     ),
                   ],
                 ),
@@ -205,6 +190,39 @@ class _BookHomeViewState extends State<BookHomeView> {
     );
   }
 
+  Row headerRow(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "Merhaba İyi Günler",
+          style: context.textTheme.headlineSmall?.copyWith(
+            color: AppColors().white,
+          ),
+        ),
+        const Spacer(),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.sort,
+            color: AppColors().white,
+          ),
+        ),
+        IconButton(
+          padding: const EdgeInsets.only(left: 16),
+          onPressed: () {},
+          icon: Icon(
+            Icons.manage_search,
+            color: AppColors().white,
+          ),
+        ),
+        SizedBox(
+          width: context.dynamicWidth(0.06),
+        )
+      ],
+    );
+  }
+
   AppBar _buildAppBar() {
     return AppBar(
       backgroundColor: Colors.transparent,
@@ -212,7 +230,9 @@ class _BookHomeViewState extends State<BookHomeView> {
       centerTitle: true,
       title: Text(
         AppStrings.discover,
-        style: context.textTheme.headlineSmall?.copyWith(color: Colors.black),
+        style: context.textTheme.headlineSmall?.copyWith(
+          color: AppColors().white,
+        ),
       ),
     );
   }
---
+++ lib/product/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  Color get purple => Color(0xff6e4bf5);
+
+  Color get pink => Color(0xffea489f);
+  Color get orange => Color(0xfff5735f);
+  Color get yellow => Color(0xfffdd776);
+  Color get white => Colors.white;
+  Color get background => Color(0xff0e0e14);
+  Color get transparentWhite => Color.fromARGB(20, 255, 255, 255);
+  Color get lightWhite => Color.fromARGB(50, 255, 255, 255);
+  Color get darkBlue => Color.fromARGB(255, 34, 34, 49);
+}

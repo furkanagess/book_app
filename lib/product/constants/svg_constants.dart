338947451ba2ed65182bcaeb0d80aab0753e4e8c
furkanagess <caglarrfurkann@gmail.com>
2023-06-18 20:03:30 +0300

    svg added

---
+++ lib/product/constants/svg_constants.dart
@@ -0,0 +1,12 @@
+class SVGConstants {
+  SVGConstants._init();
+  static SVGConstants? _instace;
+
+  static SVGConstants get instance => _instace ??= SVGConstants._init();
+
+  String toSVG(String name) => "assets/svg/$name.svg";
+
+  String get bookCategory => toSVG("book_category");
+  String get bookFavorite => toSVG("book_favorite");
+  String get bookLover => toSVG("book_lover");
+}

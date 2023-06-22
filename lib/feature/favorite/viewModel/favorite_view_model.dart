a849b56e3a474b8cf07d3e1af24dee62da788bba
furkanagess <caglarrfurkann@gmail.com>
2023-06-22 15:12:28 +0300

    favorite provider updated

--- lib/feature/favorite/viewModel/favorite_view_model.dart
+++ lib/feature/favorite/viewModel/favorite_view_model.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:book_app/product/base/base_view_model.dart';
+import 'package:book_app/product/models/book.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
@@ -8,10 +9,23 @@ part 'favorite_view_model.g.dart';
 
 class FavoriteViewModel = _FavoriteViewModelBase with _$FavoriteViewModel;
 
-abstract class _FavoriteViewModelBase with Store, BaseViewModel {
+abstract class _FavoriteViewModelBase with Store, BaseViewModel, ChangeNotifier {
   @override
   void setContext(BuildContext context) => viewModelContext = context;
 
   @override
   void init() {}
+  final List<Book> _books = [];
+
+  List<Book> get books => _books;
+
+  void addBook(Book book) {
+    _books.add(book);
+    notifyListeners();
+  }
+
+  void removeBook(Book book) {
+    _books.remove(book);
+    notifyListeners();
+  }
 }

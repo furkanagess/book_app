9893403c8a50b29cc65d1b43548d7625a38ef776
furkanagess <caglarrfurkann@gmail.com>
2023-06-27 01:47:54 +0300

    color and pubspec fixes

--- lib/product/constants/app_colors.dart
+++ lib/product/constants/app_colors.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 class AppColors {
   const AppColors._();
   static Color get green => const Color(0xff1db954);
-  static Color get white => Colors.white;
+  static Color get white => const Color(0xFFFFFFFF);
   static Color get background => const Color(0xff121212);
   static Color get darkWhite => const Color(0xff212121);
   static Color get grey => const Color(0xffb3b3b3);
   static Color get darkGrey => const Color(0xff535353);
-  static Color get transparent => Colors.transparent;
+  static Color get transparent => const Color(0x00000000);
 }
